66a036f1f3b7d7591dba472c7eede40996f85eae
enpitsu882 <enpitsu882@hotmail.co.jp>
2023-06-13 19:08:21 +0900

    現在の手番をわかりやすく表示

--- src/App.js
+++ src/App.js
@@ -73,16 +73,27 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
-    if (move > 0) {
+    if (move === currentMove) {
+      description = 'You are at move #' + move;
+    } else if (move > 0) {
       description = 'Go to move #' + move;
     } else {
       description = 'Go to game start';
     }
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    );
+    if (move === currentMove) {
+      return (
+        <li key={move}>
+          <p>{description}</p>
+        </li>
+      );
+    } else {
+      return (
+        <li key={move}>
+          <button onClick={() => jumpTo(move)}>{description}</button>
+        </li>
+      );
+    }
+
   });
 
   return (
